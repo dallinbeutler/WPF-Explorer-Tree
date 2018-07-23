40baf7f8109725ea2ceea05c59c383f8e8de5b6d
Dallin Beutler <BeutlerD@corp.lcl>
2018-07-23 15:06:46 -0600

    created a Gemini Module encapsulating the concept

---
+++ Gemini.Modules.Explorer/Modules/Module.cs
@@ -0,0 +1,21 @@
+﻿using Gemini.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using Gemini.Modules.Explorer.Modules.SolutionExplorer.ViewModels;
+
+namespace Gemini.Modules.Explorer
+{
+   [Export(typeof(IModule))]
+   class Module : ModuleBase
+   {
+      public override void Initialize()
+      {
+         base.Initialize();
+         Shell.Tools.Add(new SolutionExplorerViewModel());
+      }
+   }
+}
---
+++ Gemini.Modules.Explorer/Modules/SolutionExplorer/ViewModels/SolutionExplorerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Gemini.Framework;
+using Gemini.Framework.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gemini.Modules.Explorer.Modules.SolutionExplorer.ViewModels
+{
+   class SolutionExplorerViewModel : Tool
+   {
+      public override PaneLocation PreferredLocation => PaneLocation.Left;
+      public string Location { get; set; } = @"C:\Program Files (x86)";
+   }
+}
